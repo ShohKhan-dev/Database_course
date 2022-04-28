79c3ce944b31b4b425ba226a46778fec888b437d
Shohjahon/ Khan <71335668+ShohKhan-dev@users.noreply.github.com>
2022-04-28 17:51:04 +0500

    uploading exercises

--- lab11/exercise_4.py
+++ lab11/exercise_4.py
@@ -1,1 +1,11 @@
-print('hello')
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017")
+
+
+db = client['mydatabase']
+
+collection = db["mydatabse"]
+
+first = collection.delete_one({"borough":"Manhattan"})
+second = collection.delete_many({"cuisine":"Thai"})
